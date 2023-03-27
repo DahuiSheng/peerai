c3f6d84dbb0c82f6380e36b20f9610a018c79204
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-03-27 18:49:41 +0900

    make theme.ts

---
+++ styles/theme.ts
@@ -0,0 +1,23 @@
+
+
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    body: 'Roboto, sans-serif',
+  },
+  colors: {
+    purple: {
+      50: '#f5e1ff',
+      100: '#e3b8ff',
+      200: '#d193ff',
+      300: '#bc6cff',
+      400: '#a54bff',
+      500: '#8e3aff',
+      600: '#7729ff',
+      700: '#5f19ff',
+      800: '#4708ff',
+      900: '#3000f5',
+    },
+  },
+})
