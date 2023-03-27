84d8983f4adeea17b3ed8ef56e46ebceb59a70f9
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-03-27 17:53:48 +0900

    make LanguageSelector.tsx

---
+++ components/LanguageSelector.tsx
@@ -0,0 +1,43 @@
+import { FormControl, FormLabel, Radio, RadioGroup, VStack } from "@chakra-ui/react"
+
+type LanguageSelectorProps = {
+    languages: Language[]
+    selectedLanguage: string
+    onChange: (selectedLanguage: string) => void
+}
+
+export const LanguageSelector = ({
+    languages,
+    selectedLanguage,
+    onChange,
+}: LanguageSelectorProps) => {
+    return (
+        <VStack
+            align="left"
+        >
+            <FormControl>
+                <FormLabel
+                    fontWeight="bold"
+                    htmlFor="languages"
+                >
+                    Select a programing language
+                </FormLabel>
+                <RadioGroup
+                    colorScheme="purple"
+                    id="language"
+                    value={selectedLanguage}
+                    onChange={(value) => onChange(value.toString())}
+                >
+                    {languages.map((lang) => (
+                        <Radio
+                            key={lang.id}
+                            value={lang.id}
+                        >
+                            {languages.label}
+                        </Radio>
+                    ))}
+                </RadioGroup>
+            </FormControl>
+        </VStack>
+    )
+}
