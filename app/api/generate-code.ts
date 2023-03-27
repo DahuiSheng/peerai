427b4e9eb1aa0fb844829ec03011182e749f9f53
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-03-27 18:48:53 +0900

    make generate-code.ts

---
+++ app/api/generate-code.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const { language, functions } = req.query as { language: string, functions: string[] };
+    const code = await generateCode(language, functions);
+    res.status(200).json({ code });
+}
